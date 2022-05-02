9e07beedc76bea0284c0a81838d2a6fce63dcb7d
manuelzegarra <mzegarra@gmail.com>
2022-05-01 22:28:31 -0500

    update ksql

--- transfer-api/src/main/java/com/example/types/Transaction.java
+++ transfer-api/src/main/java/com/example/types/Transaction.java
@@ -3,6 +3,8 @@ package com.example.types;
 
 import java.time.ZonedDateTime;
 import javax.annotation.processing.Generated;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -23,6 +25,10 @@ public class Transaction {
     private String destinationId;
     @JsonProperty("amount")
     private Double amount;
+
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    //yyy-MM-dd'T'HH:mm:ssZ
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
     @JsonProperty("created")
     private ZonedDateTime created;
 
